aab9900326a74df9610ed5bb42687b8c847c4125
lurdharry <azeezhammed57@gmail.com>
2026-02-19 14:30:39 +0100

    chore: add user table sql script

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users(
+      id  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      name    VARCHAR(225) NOT NULL,
+      email  VARCHAR(225) UNIQUE NOT NULL,
+      date_of_birth  DATE,
+      password_hash VARCHAR(225) NOT NULL,
+      created_at  TIMESTAMP NOT NULL DEFAULT now(),
+      updated_at  TIMESTAMP NOT NULL DEFAULT now()
+);
