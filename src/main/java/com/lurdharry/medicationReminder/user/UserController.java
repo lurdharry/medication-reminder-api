1a1673a78356e623f41edf8e2bc10abfdfccd794
lurdharry <azeezhammed57@gmail.com>
2026-02-25 23:55:59 +0100

    chore: get user profile

--- src/main/java/com/lurdharry/medicationReminder/user/UserController.java
+++ src/main/java/com/lurdharry/medicationReminder/user/UserController.java
@@ -1,32 +1,31 @@
 package com.lurdharry.medicationReminder.user;
 
 import com.lurdharry.medicationReminder.exception.dto.ResponseDTO;
-import com.lurdharry.medicationReminder.user.dto.UserRequest;
-import jakarta.validation.Valid;
+import com.lurdharry.medicationReminder.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
+    private final UserMapper mapper;
 
-    @PostMapping("/createUser")
-    public ResponseEntity<ResponseDTO> createUser(
-            @RequestBody @Valid UserRequest request
-    ) {
-
-        var user = userService.createUser(request);
-
-        ResponseDTO res = new ResponseDTO(HttpStatus.CREATED.value(), "User Created", user);
-
-        return  new ResponseEntity<>(res, HttpStatus.CREATED);
-
+    @GetMapping("/me")
+    public ResponseEntity<ResponseDTO> getProfile (
+            @AuthenticationPrincipal User user
+    ){
+        var response = userService.getProfile(user);
+        ResponseDTO res = new ResponseDTO(HttpStatus.OK.value(), "Profile retrieved", response);
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
+
 }
--- src/main/java/com/lurdharry/medicationReminder/user/UserService.java
+++ src/main/java/com/lurdharry/medicationReminder/user/UserService.java
@@ -1,10 +1,8 @@
 package com.lurdharry.medicationReminder.user;
 
-import com.lurdharry.medicationReminder.exception.ResponseException;
 import com.lurdharry.medicationReminder.user.dto.UserResponse;
-import com.lurdharry.medicationReminder.user.dto.UserRequest;
+import com.lurdharry.medicationReminder.user.model.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,20 +16,8 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
 
-    public UserResponse createUser(UserRequest userRequest){
-
-        var existingUser = userRepository.findByEmail(userRequest.email());
-
-        if (existingUser.isPresent()) {
-            throw new ResponseException("Email already exists", HttpStatus.CONFLICT);
-        }
-
-        String passwordHash = passwordEncoder.encode(userRequest.password());
-        var user =  mapper.toUser(userRequest);
-        user.setPasswordHash(passwordHash);
-
-        userRepository.save(user);
-
+    public UserResponse getProfile(User user) {
         return mapper.toUserResponse(user);
     }
+
 }
