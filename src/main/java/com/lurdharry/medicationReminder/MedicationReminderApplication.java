509673bd6d04ae0e8847698199380305f703042c
Adekunle <azeezhammed57@gmail.com>
2026-03-30 14:11:59 +0100

    chore: configure OpenAPI docs with JWT bearer auth security scheme

--- src/main/java/com/lurdharry/medicationReminder/MedicationReminderApplication.java
+++ src/main/java/com/lurdharry/medicationReminder/MedicationReminderApplication.java
@@ -1,11 +1,26 @@
 package com.lurdharry.medicationReminder;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+)
+@OpenAPIDefinition(
+		info = @Info(title = "Medication Reminder API", version = "1.0"),
+		security = @SecurityRequirement(name = "bearerAuth")
+)
 public class MedicationReminderApplication {
 
 	public static void main(String[] args) {
